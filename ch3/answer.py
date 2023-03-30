b7ec235ce402625c304279f4b48aafcc445eb26c
JongYoon Won <64014435+zibb03@users.noreply.github.com>
2023-03-30 11:37:02 +0900

    실습 자료
    오류 수정한 파일입니다.

--- ch3/answer.py
+++ ch3/answer.py
@@ -1,64 +1,84 @@
-contacts = {}
+contacts = []
 count = 0
 
-def add_contact():
+def add_contact(contacts, count):
     name = input("주소록에 추가할 이름을 입력하세요: ")
 
-    for i in contacts:
-        if i == name:
-            print("이미 존재합니다.")
-            return
+    for i in range(count):
+        if contacts[i][0] == name:
+            print("이미 존재합니다.\n")
+            return contacts, count
     
     number = input("전화번호를 입력하세요: ")
+    if len(number) == 12:
+        print("올바르지 않은 번호입니다.\n")
+        return contacts, count
+    
     company = input("회사 정보를 입력하세요: ")
     
+    contacts.append([])
     contacts[count].append(name)
     contacts[count].append(number)
     contacts[count].append(company)
 
-    print("Contact added successfully.")
+    print("연락처가 추가되었습니다.\n")
     count += 1
 
-def update_contact():
+    return contacts, count
+
+def update_contact(contacts, count):
     name = input("수정할 이름을 입력하세요: ")
 
-    for i in contacts:
-        if i == name:
+    for contact in contacts:
+        if contact[0] == name:
             number = input("수정할 전화번호를 입력하세요: ")
+            if len(number) == 12:
+                print("올바르지 않은 번호입니다.\n")
+                return contacts, count
+            
             company = input("수정할 회사 정보를 입력하세요: ")
             
-            contacts[count][0] = name
-            contacts[count][1] = number
-            contacts[count][2] = company
+            contact[0] = name
+            contact[1] = number
+            contact[2] = company
 
-            print("수정 완료.")
-            return
+            print("수정 완료.\n")
+            return contacts, count
 
-    print("해당 이름이 주소록에 존재하지 않습니다.")
+    print("해당 이름이 주소록에 존재하지 않습니다.\n")
 
-def delete_contact():
+    return contacts, count
+
+def delete_contact(contacts, count):
     name = input("삭제할 이름을 입력하세요: ")
 
-    for i in contacts:
-        if i == name:
-            del contacts[name]
-            print("삭제 완료.")
-            return
-
-    print("해당 이름이 주소록에 존재하지 않습니다.")
-
-def search_contact():
-    name = input("삭제할 이름을 입력하세요: ")
-    for i in contacts:
-        if i == name:
-            print("이름: ", contacts[count][0], "전화번호: ", contacts[count][1], "회사: ", contacts[count][2])
-            return
-        
-    print("존재하지 않는 이름입니다.")
-
-def display_all_contacts():
     for i in range (count):
-        print("이름: ", contacts[count][0], "전화번호: ", contacts[count][1], "회사: ", contacts[count][2])
+        if contacts[i][0] == name:
+            del contacts[i]
+            print("삭제 완료.\n")
+            count -= 1
+            return contacts, count
+
+    print("해당 이름이 주소록에 존재하지 않습니다.\n")
+
+    return contacts, count
+
+def search_contact(contacts, count):
+    name = input("탐색할 이름을 입력하세요: ")
+    for contact in contacts:
+        if contact[0] == name:
+            print("이름:", contact[0], "전화번호:", contact[1], "회사:", contact[2], "\n")
+            return 
+    
+    print("존재하지 않는 이름입니다.\n")
+
+
+def display_all_contacts(contacts, count):
+    for i in range (count):
+        print("이름:", contacts[i][0], "전화번호:", contacts[i][1], "회사:", contacts[i][2])
+        if i == (count - 1):
+            print()
+
 
 while True:
     print("주소록")
@@ -67,19 +87,20 @@ while True:
     print("3. 주소록 정보 제거")
     print("4. 주소록 정보 찾기")
     print("5. 주소록 모두 출력")
-    print("6. 종료")
+    print("6. 종료\n")
     choice = input("실행할 명령 번호를 입력하세요: ")
     if choice == "1":
-        add_contact()
+        contacts, count = add_contact(contacts, count)
     elif choice == "2":
-        update_contact()
+        contacts, count = update_contact(contacts, count)
     elif choice == "3":
-        delete_contact()
+        contacts, count = delete_contact(contacts, count)
     elif choice == "4":
-        search_contact()
+        search_contact(contacts, count)
     elif choice == "5":
-        display_all_contacts()
+        display_all_contacts(contacts, count)
     elif choice == "6":
+        print("주소록이 종료됩니다.")
         break
     else:
         print("오류: 유효하지 않은 번호입니다.")
