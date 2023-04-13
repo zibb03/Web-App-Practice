39d849d1c7b4abd10a9a51c7ce925b2f765babc1
JongYoon Won <64014435+zibb03@users.noreply.github.com>
2023-04-13 13:39:52 +0900

    실습 자료

---
+++ ch3/3.py
@@ -0,0 +1,41 @@
+# a = 93
+# b = 55
+# if a > b:
+#     print(b)
+# print(a) 
+
+# a = 10
+# b = 27
+# if a >= b:
+#     print(a)
+# else:
+#     print(b)
+
+# a = 27
+# b = 27
+# if a >b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a is equal to b")
+
+# a = 93
+# b = 27
+# if a > b:
+#     print("a is greater than b")
+# elif a < b:
+#     print("a is less than b")
+# else: 
+#     print ("a is equal to b")
+
+# a = 16
+# b = 25
+# c = 27
+# if a > b:
+#     if b > c:
+#         print ("a is greater than b and b is greater than c")
+#     else: 
+#         print ("a is greater than b and less than c")
+# elif a == b:
+#     print ("a is equal to b")
+# else:
+#     print ("a is less than b")
---
+++ ch3/4.py
@@ -0,0 +1,13 @@
+x = 1
+
+while x < 5:
+    print(x)
+    x += 1
+
+if x == 5:
+    print("x equals 5")
+
+# countdown = [4, 3, 2, 1, 0]
+# for number in countdown:
+#     print(number)
+# print("Blast off!! 🚀")
