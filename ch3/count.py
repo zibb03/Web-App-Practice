6db34119a3df0e24b239d8a2cfe5a47df5d5c743
JongYoon Won <64014435+zibb03@users.noreply.github.com>
2023-04-24 15:21:31 +0900

    실습 정답

---
+++ ch3/count.py
@@ -0,0 +1,9 @@
+# 리스트의 수를 출력하는 문제
+
+# number 리스트를 정의하는 코드 
+number = [1, 2, 3, 4, 5]
+
+# number 리스트의 값을 하나씩 꺼내 n에 넣는 for 반복문
+for n in number:
+    # 변수 n의 값을 출력하는 print문
+    print(n)
---
+++ ch3/number.py
@@ -0,0 +1,12 @@
+# 인자 값이 양수일 경우, 출력하는 문제
+
+# number라는 인자를 받아 실행되는 judge 함수
+def judge(number):
+    # number 변수의 값이 양수일 때
+    if number > 0:
+        # number 변수의 값을 출력
+        print(number)
+
+# 정의한 judge 함수를 실행하는 코드
+# 인자를 전달해 주지 않으면 실행되지 않음
+judge(97235)
