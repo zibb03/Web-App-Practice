80f88f7f5e5036b502ec719ff06a5df3b2837612
JongYoon Won <64014435+zibb03@users.noreply.github.com>
2023-05-09 20:54:31 +0900

    실습 자료

--- ch3/1_datatype.py
+++
@@ -1,7 +0,0 @@
-print("Hello world") # Hello world
-print(1) # 1
-
-print(2 + 2) # 4
-print(2 - 1) # 1
-print(2 * 3) # 6
-print(6 / 2) # 3.0
--- ch3/4_boolean.py
+++
@@ -1,6 +0,0 @@
-print(True) # True
-print(False) # False
-print(1 > 2) # False
-print(1 < 2) # True
-print(1 == 1) # True
-print(1 != 1) # False
